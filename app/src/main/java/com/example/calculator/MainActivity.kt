04abc8336ebd056b93f3eec484780b3d2f711dcc
Oleksandra <alexadrobot@gmail.com>
2023-03-08 22:34:36 +0200

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+private var button: Button? = null
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.buttonForSolve)
+
+        fun setTextFields(str: String){
+    }
     }
 }
