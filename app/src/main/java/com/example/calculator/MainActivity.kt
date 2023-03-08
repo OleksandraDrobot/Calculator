8e5dbbc85cdbd0ef5bea609640b8c5089ea099c0
Oleksandra <alexadrobot@gmail.com>
2023-03-08 23:25:13 +0200

    circle ui done

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,18 +2,29 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-private var button: Button? = null
-
 class MainActivity : AppCompatActivity() {
+
+    private var button: Button? = null
+    private var input: EditText? = null
+    private var resultOf: TextView? = null
+    private val pi = 3.14
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.buttonForSolve)
+        button = findViewById(R.id.buttonForSolve1)
+        input = findViewById(R.id.EnterNumberOfCircle)
+        resultOf = findViewById(R.id.ResultOfWork)
 
-        fun setTextFields(str: String){
-    }
+        button?.setOnClickListener {
+            val radius = input?.text.toString().toDouble()
+            val s = pi*radius*radius
+
+            resultOf?.text = "result = $s"
+        }
     }
 }
